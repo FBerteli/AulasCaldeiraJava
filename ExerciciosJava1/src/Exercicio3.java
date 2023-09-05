1d9a733dd0edb118eacf461a2996311e04a7700d
Felipe Berteli <112442989+FBerteli@users.noreply.github.com>
2023-09-05 16:45:23 -0300

    Exercicios01

---
+++ ExerciciosJava1/src/Exercicio3.java
@@ -0,0 +1,9 @@
+public class Exercicio3 {
+    public static void main(String[] args){
+        int n1 = 15;
+        int n2 = 13;
+        int n3 = 23;
+
+        System.out.printf("O maior numero é " + n3 + ", O menor numero é " + n2);
+    }
+}
---
+++ ExerciciosJava1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 23;
+        System.out.printf("Meu nome é Felipe Berteli!! Olá mundo!! e eu tenho " + idade + " Anos");
+
+        }
+    }
