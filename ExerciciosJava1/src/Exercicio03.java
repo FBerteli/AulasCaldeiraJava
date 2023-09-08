b4f101beea1e925ecdfe4d03bdad80d611d97969
Felipe Berteli <felipeberteli@hotmail.com>
2023-09-08 18:07:34 -0300

    Atualizações nas atividades

--- ExerciciosJava1/src/Exercicio03.java
+++ ExerciciosJava1/src/Exercicio03.java
@@ -1,9 +1,11 @@
 public class Exercicio03 {
-    public static void main(String[] args){
-        int n1 = 15;
-        int n2 = 13;
-        int n3 = 23;
+    public static void main(String... args){
+        int n1 = 6;
+        int n2 = 18;
+        int n3 = 22;
+        int media = n1+n2+n3/3;
+        System.out.printf("O primeiro numero é %d, o segundo numero é %d, o terceiro numero é %d%n", n1, n2, n3);
+        System.out.println("O maior número é o " + n3 + ", o menor número é "+ n1 +  " e a média destes números é " + media);
 
-        System.out.printf("O maior numero é " + n3 + ", O menor numero é " + n2);
     }
 }
--- ExerciciosJava1/src/exercicio04.java
+++ ExerciciosJava1/src/exercicio04.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class exercicio04 {
-    public static void main (String[] args){
-    double SalarioMin = 1320;
-    double SalarioUser = 2640;
-    int ContadorSalario = 0;
+    public static void main(String[] args) {
 
-    System.out.printf("O salário mínimo atual é " + SalarioMin);
+        double salarioMinimo = 1320.0; // Em reais
 
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Digite o valor do seu salário: ");
+        double salarioUsuario = scanner.nextDouble();
+
+
+        double quantidadeSalariosMinimos = salarioUsuario / salarioMinimo;
+
+
+        System.out.println("Você ganha aproximadamente " + quantidadeSalariosMinimos + " salários mínimos.");
+
+
+        scanner.close();
     }
 }
