5c3c6f7cc423a16d65b14f2f0881d850d5e39129
Felipe Berteli <felipeberteli@hotmail.com>
2023-09-11 14:35:56 -0300

    Atualizações nas atividades

---
+++ ExerciciosJava1/src/Exercicio07.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exercicio07 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a sua idade: ");
+        double idadeUsuario = scanner.nextInt();
+
+        System.out.print("Digite o seu salário: ");
+        double salarioUsuario = scanner.nextDouble();
+
+        if (idadeUsuario>18&&salarioUsuario>2000){
+            System.out.println("Usuário pode comprar um automóvel");
+        }
+        else {
+            System.out.println("Usuário não pode comprar um automóvel!!");
+        }
+    }
+}
---
+++ ExerciciosJava1/src/Exercicio08.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Exercicio08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Gestante");
+            System.out.println("2. Idosa");
+            System.out.println("3. PCD (Pessoa com Deficiência)");
+            System.out.println("4. Nenhuma das alternativas");
+            System.out.println("0. Sair");
+
+            System.out.print("Digite o número da opção: ");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Você tem direito a fila prioritária como gestante.");
+                    break;
+                case 2:
+                    System.out.println("Você tem direito a fila prioritária como pessoa idosa.");
+                    break;
+                case 3:
+                    System.out.println("Você tem direito a fila prioritária como pessoa com deficiência.");
+                    break;
+                case 4:
+                    System.out.println("Você não tem direito a fila prioritária.");
+                    break;
+                case 0:
+
+                    System.out.println("Programa encerrado.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            }
+        }
+    }
+}
