b9dbeed80159fc22c12d72c8bb308e494b561490
Felipe Berteli <felipeberteli@hotmail.com>
2023-09-12 16:53:37 -0300

    Atualização pastas

--- ExerciciosJava1/src/Aula2/Exercicio01.java
+++ ExerciciosJava1/src/Aula2/Exercicio01.java
@@ -1,4 +1,27 @@
 package Aula2;
-
+import java.util.Scanner;
 public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite dois números primos por vez, com um intervalo entre eles");
+        int numeroInicial = input.nextInt();
+        int numeroFinal = input.nextInt();
+
+
+            for (int i = numeroInicial; i < numeroFinal; i++) {
+                if (ehPrimo(i))
+                    System.out.println(i + " é um número Primo");
+            }
+            input.close();
+        }
+    private static boolean ehPrimo(int numero) {
+        for (int j = 2; j < numero; j++) {
+            if (numero % j == 0)
+                return false;
+        }
+        return true;
+    }
+
+
 }
+
